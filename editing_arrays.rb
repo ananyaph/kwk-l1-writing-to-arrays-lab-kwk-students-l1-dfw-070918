42086f8009ee6c660be273a1895b5edc5072b270
Ananya Phadke <phadkeananya@gmail.com>
2018-07-10 20:53:38 +0000

    automatically backed up by learn

--- editing_arrays.rb
+++ editing_arrays.rb
@@ -6,18 +6,18 @@ rainbow_colors = [
 
 def change_rainbow_colors
   array = []
-  array[0] = red
-  array[1] = light_red
-  array[2] = light_yellow
+  array[0] = "red"
+  array[1] = "light_red"
+  array[2] = "light_yellow"
   array
 
 end
 
 def add_colors
   array = []
-  array[0] = red
-  array[1] = light_red
-  array[2] = light_yellow
+  array[0] = "red"
+  array[1] = "light_red"
+  array[2] = "light_yellow"
   array.push "green"
   array.push "blue"
   array
