63cf249d8d58fb9414180869d1bd3f40ca17f03a
Ananya Phadke <phadkeananya@gmail.com>
2018-07-10 20:43:26 +0000

    automatically backed up by learn

--- editing_arrays.rb
+++ editing_arrays.rb
@@ -8,6 +8,7 @@ def change_rainbow_colors
   rainbow_colors[0] = red
   rainbow_colors[1] = light_red
   rainbow_colors[2] = light_yellow
+  rainbow_colors = change_rainbow_colors
 end
 
 def add_colors
